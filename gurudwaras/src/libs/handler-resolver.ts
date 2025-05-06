22237c6abaea7663ba0ec0847ee8bc497199e7f6
Aamirdev570 <aamajeed@legitbytes.com>
2025-05-06 17:36:26 +0530

    Add Success Messages

--- gurudwaras/src/libs/handler-resolver.ts
+++ gurudwaras/src/libs/handler-resolver.ts
@@ -1,6 +1,5 @@
 import * as path from "path";
 
 export const handlerPath = (context: string) => {
-  console.log("Path",path);
   return path.relative(process.cwd(), context).replace(/\\/g, "/");
 };
